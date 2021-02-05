10145b56274a96f4b69518485e36aef453a97e64
jeffmyers81 <jeffmyers81@gmail.com>
2021-02-05 23:34:07 +0000

    Added some cool stuff for JUnit

--- src/main/java/com/simplilearn/calculator2/Calculator2Application.java
+++ src/main/java/com/simplilearn/calculator2/Calculator2Application.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator2Application {
 
+    	public static double getRandomTicketNumber(){
+   		double min=1000;
+	  	double max=9999;
+   		double x = (int)(Math.random()*((max-min)+1))+min;
+   		return x;
+	}	
+
 	public static void main(String[] args) {
 		SpringApplication.run(Calculator2Application.class, args);
 	}
--- src/test/java/com/simplilearn/calculator2/Calculator2ApplicationTests.java
+++ src/test/java/com/simplilearn/calculator2/Calculator2ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.simplilearn.calculator2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,12 @@ class Calculator2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	private TicketGeneratorApplication ticketer = new TicketGeneratorApplication();
+	@Test
+	public void testRandomNumber() {
+   		assertTrue(ticketer.getRandomTicketNumber()>1000&&ticketer.getRandomTicketNumber()<9999);
 	}
 
 }
